94d2dbc2df7812020d1b0dcce254c537d550691c
soderblom78 <soderblom78@home.se>
2024-12-23 15:32:51 +0100

    Initial commit

---
+++ lucka_1.py
@@ -0,0 +1,25 @@
+"""
+Lucka 1 - Tomtens telefonnummer
+Det var en krispig vintermorgon den första december. Snön låg som ett tjockt, fluffigt täcke över hela Nordpolen, och stjärnorna glittrade som 
+små diamanter på den mörka himlen. Nissarna, klädda i sina färgglada kläder och med rosiga kinder från den kyliga luften, skuttade glatt mot 
+tomteverkstaden. Det var äntligen dags att börja förberedelserna inför julen, den mest spännande tiden på året!
+
+Nissarna hade sett fram emot denna dag i månader. De hade planerat, drömt och pratat om alla leksaker och presenter de skulle skapa. Men när de 
+nådde fram till verkstaden, möttes de av något mycket oväntat – dörren till verkstaden var låst! Detta hade aldrig hänt förut. Verkstaden, ett 
+ställe fullt av glädje och skratt, stod nu tyst och otillgänglig framför dem.
+
+"Låt oss ringa Tomten" föreslog ena nissen. Men nissar sparar inte sina telefonnummer i sin telefon som vi vanliga människor gör. Istället minns 
+de telefonnummer med hjälp av enkla minnesregler.
+
+Minnesregeln för tomtens telefonnummer är följande:
+
+Beräkna summan av alla tal mellan 127 och 267 (inklusive både 127 och 267) adderat med summan av alla tal mellan 1110 och 1378 (inklusive både 
+1110 och 1378) adderat med summan av alla tal mellan 3239293 och 3239330 (inklusive både 3239293 och 3239330).
+
+Vad är tomtens telefonnummer?
+
+"""
+
+print(sum(list(range(127,268))) + sum(list(range(1110,1379))) + sum(list(range(3239293,3239331))))
+
+Svar: 123456250
